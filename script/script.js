8fc8c66a9ffc013da50671f18d67a0c3c7df9f31
8rice <bricesabuco@gmail.com>
2021-12-22 18:37:15 +0100

    script

--- script/script.js
+++ script/script.js
@@ -1,42 +1,56 @@
 // Create a "close" button and append it to each list item
+/* PAS UTILE C'EST JUSTE POUR LES ELEMENTS LI DEJA PRESENTS*/
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("\u00D7"); //CROIX
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
+
+
+
+/* PARTIE UTILE */
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+    var li = this.parentElement;
+    li.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
+  if (ev.target.tagName === 'DIV') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  let div =  document.createElement("div"); //juste l'élément text et pas la croix (pour le line-through)
+  let choice = document.querySelectorAll("input[type='radio']"); //get the radio inputs
+
+  choice.forEach()
+ 
+
+
+  div.appendChild(t);
+  li.appendChild(div);
+
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("list").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
